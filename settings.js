bad277acf673c197ef29476c2b47819f9b65edb1
pukigirl <112471817+pukigirl@users.noreply.github.com>
2022-08-30 14:01:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,19 +21,19 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94763427338']
-global.premium = ['94763427338']
-global.ownernomer = '94763427338'
-global.ownername = '៚ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ͢⁸⁵⁴¹⃟✦⃝̮̮〽️'
-global.botname = 'VIHANGA MD BOT'
-global.footer = 'VIHANGA MD'
-global.cap = '*Genarated By Vihanga Md*'
+global.owner = ['94722055318']
+global.premium = ['0722055318']
+global.ownernomer = '95722055318'
+global.ownername = '꧁ 𝚂𝙰𝙳ͥ𝙴𝙴  𝙽ͣ 𝙴ͫ 𝙾  ツ꧂'
+global.botname = '꧁ 𝚂𝙰𝙳ͥ𝙴𝙴  𝙽ͣ 𝙴ͫ 𝙾  ツ꧂'
+global.footer = '꧁ 𝚂𝙰𝙳ͥ𝙴𝙴  𝙽ͣ 𝙴ͫ 𝙾  ツ꧂ '
+global.cap = '*Genarated By ꧁ 𝚂𝙰𝙳ͥ𝙴𝙴  𝙽ͣ 𝙴ͫ 𝙾  ツ꧂*'
 global.ig = 'https://github.com/vihangabot'
 global.region = 'Sri Lanka, Sabaragamuwa, Embilipitiya'
 global.sc = 'https://github.com/vihangabot'
 global.myweb = 'https://chat.whatsapp.com/BDge3i6chrQKWkkSRellyT'
-global.packname = 'Vihanga Md Sticker Bot'
-global.author = '៚ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ͢⁸⁵⁴¹⃟✦⃝̮̮〽️'
+global.packname = 'Vihanga Md ꧁ 𝚂𝙰𝙳ͥ𝙴𝙴  𝙽ͣ 𝙴ͫ 𝙾  ツ꧂'
+global.author = '꧁ 𝚂𝙰𝙳ͥ𝙴𝙴  𝙽ͣ 𝙴ͫ 𝙾  ツ꧂'
 global.autobio = 'true'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
